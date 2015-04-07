3162a5b90d2d8d3624a66c42f247c913ab80f4a8
admin <admin@iwodeMac-mini.local>
2015-04-07 09:12:37 +0800

    11

--- lib/cocos2d-x/cocos2dx/platform/CCPlatformMacros.h
+++ lib/cocos2d-x/cocos2dx/platform/CCPlatformMacros.h
@@ -214,8 +214,7 @@ public: virtual void set##funName(varType var)   \
 #define CCLOGWARN(...)   do {} while (0)
 
 #elif COCOS2D_DEBUG == 1
-#define CCLOG(format, ...)
-//#define CCLOG(format, ...)      cocos2d::CCLog(format, ##__VA_ARGS__)
+#define CCLOG(format, ...)      cocos2d::CCLog(format, ##__VA_ARGS__)
 #define CCLOGERROR(format,...)  cocos2d::CCLog(format, ##__VA_ARGS__)
 #define CCLOGINFO(format,...)   do {} while (0)
 #define CCLOGWARN(...) __CCLOGWITHFUNCTION(__VA_ARGS__)
